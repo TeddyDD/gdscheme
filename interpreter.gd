d043dd27fb6e5ff5c398df9f07956955b705d293
Daniel Lewan <vision360.daniel@gmail.com>
2016-06-19 16:29:30 +0200

    add array_append_empty helper function
    This function allows to append empty array to array.
    
    [[]].append([]) # this won't work
    array_append_empty([]) # this works [[][]]

--- interpreter.gd
+++ interpreter.gd
@@ -6,4 +6,9 @@ func tokenize(program):
 	p = program.replace("(", " ( ").replace(")", " ) ").split(" ",false)
 	return Array(p)
 
+func array_append_empty(array):
+	if array == null:
+		array = []
+	array.resize(array.size() + 1)
+	array[-1] = []
 
