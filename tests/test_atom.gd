fb50dafc30d6924b0ef8f46cd1ffb6d04316a8f0
Daniel Lewan <vision360.daniel@gmail.com>
2016-06-19 16:37:19 +0200

    test atom function

---
+++ tests/test_atom.gd
@@ -0,0 +1,22 @@
+extends "res://gut.gd".Test
+
+var interpeter = preload("res://interpreter.gd")
+
+var i = null
+
+func setup():
+	i = interpeter.new()
+	
+func teardown():
+	i = null
+	
+func test_symbol():
+	gut.assert_eq(i.atom("test"), "test", "Symbol should be string")
+	
+func test_integer():
+	gut.assert_eq(i.atom("1"), 1, "Integer token should return number")
+	gut.assert_eq(typeof(i.atom("1")), TYPE_REAL, "Integer shoult return float")
+
+func test_float():
+	gut.assert_eq(i.atom("1.5"), 1.5, "Float token shoul return float")
+	gut.assert_eq(str(i.atom("1.5")), "1.5")
