af199de223f6d27ba9312c913dd81cef281fe221
Daniel Lewan <vision360.daniel@gmail.com>
2016-06-20 14:05:44 +0200

    prevent + to be evaluated as int by atom
    it seems in Godot "+".is_valid_integer() == true but
    "+".is_valid_float == false

--- interpreter.gd
+++ interpreter.gd
@@ -38,7 +38,7 @@ func array_append_empty(array):
 	array[-1] = []
 
 func atom(token):
-	if token.is_valid_integer():
+	if token.is_valid_integer() and token.is_valid_float():
 		return token.to_int()
 	elif token.is_valid_float():
 		return token.to_float()
