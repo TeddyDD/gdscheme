993e9d7ea2a48ffb702d17eb34c4d7ac617320d0
Daniel Lewan <daniel@teddydd.me>
2024-11-03 18:21:48 +0100

    format

--- lisp.gd
+++ lisp.gd
@@ -3,31 +3,40 @@ extends RefCounted
 
 var global_env = standard_env()
 
+
 func run(program: String):
 	return eval(parse(program))
+
 
 func parse(program: String):
 	return read_from_tokens(tokenize(program))
 
+
 func tokenize(program: String) -> Array:
-	var res: Array = program.\
-		replace("\t", "").\
-		replace("\n", "").\
-		replace("(", " ( ").\
-		replace(")", " ) ").split(" ", false)	
+	var res: Array = (
+		program
+		. replace("\t", "")
+		. replace("\n", "")
+		. replace("(", " ( ")
+		. replace(")", " ) ")
+		. split(" ", false)
+	)
 	return res
+
 
 var depth := -1
 
-func eval(x, env:Env=global_env):
+
+func eval(x, env: Env = global_env):
 	depth += 1
 	print(" ".repeat(depth * 2), "-> eval: ", schemestr(x))
 	var res = eval_impl(x, env)
 	print(" ".repeat(depth * 2), "<- ", schemestr(res))
 	depth -= 1
 	return res
-	
-func eval_impl(x, env:Env=global_env):
+
+
+func eval_impl(x, env: Env = global_env):
 	if x is String:
 		return env.find(x)
 	if x is float or x is int:
@@ -43,7 +52,7 @@ func eval_impl(x, env:Env=global_env):
 		["define", var symbol, var exp]:
 			env.update(symbol, eval(exp, env))
 			return
-		['halt', var body]:
+		["halt", var body]:
 			breakpoint
 			return eval(body, env)
 	var proc = eval(x[0], env)
@@ -53,16 +62,18 @@ func eval_impl(x, env:Env=global_env):
 	var res = apply(proc, args)
 	return res
 
+
 func apply(proc, args):
 	if proc is Callable:
 		return proc.call(args)
 	elif proc is Proc:
 		return proc.run(args)
 
+
 func read_from_tokens(tokens: Array) -> Array:
 	var prev_stack = []
 	var current = []
-	
+
 	for t in tokens:
 		match t:
 			"(":
@@ -79,8 +90,9 @@ func read_from_tokens(tokens: Array) -> Array:
 	if current.is_empty():
 		return current
 	else:
-		assert(current.size()==1)
+		assert(current.size() == 1)
 		return current[0]
+
 
 func atom(token):
 	if token.is_valid_int():
@@ -90,41 +102,52 @@ func atom(token):
 	else:
 		return token
 
+
 class Proc:
 	var body
 	var params: Array
 	var env: Env
 	var eval: Callable
+
 	func _init(params, body, env, eval) -> void:
 		self.body = body
 		self.params = params
 		self.env = env
 		self.eval = eval
+
 	func _to_string() -> String:
 		return "<proc %s>" % [params]
+
 	func run(args):
 		var newEnv := Env.new()
 		newEnv.outer = self.env
 		newEnv.add_params(self.params, args)
 		return eval.call(body, newEnv)
 
+
 class Env:
 	var content := {}
 	var outer: Env
-	func _init(from:Dictionary = {}, outer=null, params=[], args=[]):
+
+	func _init(from: Dictionary = {}, outer = null, params = [], args = []):
 		self.content = from
 		self.outer = outer
 		self.add_params(params, args)
+
 	func add_params(params: Array, args: Array) -> void:
 		for i in range(params.size()):
 			update(params[i], args[i])
-	func update(k,v):
+
+	func update(k, v):
 		content[k] = v
+
 	func find(v):
-		return self.content[v] if content.has(v) else outer.find(v) 
+		return self.content[v] if content.has(v) else outer.find(v)
+
 
 func rest(a: Array) -> Array:
 	return a.slice(1, len(a))
+
 
 func standard_env() -> Env:
 	var env = {
@@ -139,10 +162,10 @@ func standard_env() -> Env:
 		"=": func(x): return _equal(x[0], x[1]),
 		"map": func(x): return _map(x[0], x[1]),
 		"cons": func(x): return _cons(x[0], x[1]),
-		"+":  func(x): return x.reduce(_sum),
-		"-": func(x): return x.reduce(func(a,x): return a-x),
-		"*": func(x): return x.reduce(func(a,x): return a*x),
-		"/": func(x): return x.reduce(func(a,x): return a/x),
+		"+": func(x): return x.reduce(_sum),
+		"-": func(x): return x.reduce(func(a, x): return a - x),
+		"*": func(x): return x.reduce(func(a, x): return a * x),
+		"/": func(x): return x.reduce(func(a, x): return a / x),
 		">": func(x): return x[0] > x[1],
 		"<": func(x): return x[0] < x[1],
 		">=": func(x): return x[0] >= x[1],
@@ -150,27 +173,33 @@ func standard_env() -> Env:
 	}
 	return Env.new(env)
 
-func _cons(x,y):
+
+func _cons(x, y):
 	var res = [x]
 	for i in y:
 		res.append(i)
 	return res
 
+
 func _map(fn, list):
 	return list.map(func(elm): return apply(fn, [elm]))
 
+
 func _sum(accum, number):
 	#if accum == null:
-		#return number
+	#return number
 	return accum + number
 
-func _equal(a,b) -> bool:
+
+func _equal(a, b) -> bool:
 	return typeof(a) == typeof(b) and a == b
+
 
 func _list(x):
 	var r = []
 	r.append_array(x)
 	return r
+
 
 func schemestr(x):
 	match typeof(x):
--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var lisp = Lisp.new()
 @onready var env = lisp.global_env
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#var l = Lisp.new()
@@ -11,12 +12,15 @@ func _ready() -> void:
 	env.update("print", stdout)
 	%Input.grab_focus()
 
-func  stdout(x):
+
+func stdout(x):
 	%Output.text = %Output.text + "\n" + lisp.schemestr(x)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 
 func _on_input_text_submitted(new_text: String) -> void:
 	var res = lisp.eval(lisp.parse(new_text), lisp.global_env)
